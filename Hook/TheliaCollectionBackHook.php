964c1c0a5fc3d7215e473a09f5c7843345b007fb
lnormandon <lnormandon@openstudio.fr>
2024-01-16 15:00:14 +0100

    Add Hook tab on Product, Category & Folder

--- Hook/TheliaCollectionBackHook.php
+++ Hook/TheliaCollectionBackHook.php
@@ -9,20 +9,32 @@ use TheliaCollection\TheliaCollection;
 
 class TheliaCollectionBackHook extends BaseHook
 {
-    public function onProductTab(HookRenderBlockEvent $event): void
-    {
-    }
-
-    public function onCategoryTab(HookRenderBlockEvent $event): void
-    {
-    }
-
     public function onModuleConfig(HookRenderEvent $event)
     {
         $event->add($this->render('thelia-collection-configuration.html'));
     }
 
+    public function onProductTab(HookRenderBlockEvent $event): void
+    {
+        $this->renderHookTab($event, 'product');
+    }
+
+    public function onCategoryTab(HookRenderBlockEvent $event): void
+    {
+        $this->renderHookTab($event, 'category');
+    }
+
     public function onContentTab(HookRenderBlockEvent $event): void
+    {
+        $this->renderHookTab($event, 'content');
+    }
+
+    public function onFolderTab(HookRenderBlockEvent $event): void
+    {
+        $this->renderHookTab($event, 'folder');
+    }
+
+    private function renderHookTab(HookRenderBlockEvent $event, string $itemType): void
     {
         $itemId = $event->getArgument('id');
         $event->add(
@@ -33,14 +45,10 @@ class TheliaCollectionBackHook extends BaseHook
                     'manage-collection.html',
                     [
                         'itemId' => $itemId,
-                        'itemType' => 'content',
+                        'itemType' => $itemType,
                     ]
                 ),
             ]
         );
-    }
-
-    public function onFolderTab(HookRenderBlockEvent $event): void
-    {
     }
 }
